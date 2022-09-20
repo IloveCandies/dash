19e4a6d5c43c0ab65669eb511fbb2ff8a68c81f2
Maksim Patsulo <patsulo.m@outlook.com>
2022-09-20 19:55:44 +0700

    upload

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ flask_app = Flask(__name__,template_folder="templates")
 flask_app.config['SECRET_KEY'] = '121wqewq232131212232312wdq3213qwq'
 
 from db.base import *
-
-from admin.auth import *
+from admin.controllers.auth import *
+from admin.controllers.view_controller import *
+from admin.views import *
 
 if __name__ == '__main__':
     flask_app.run(debug=True)
